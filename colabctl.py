7ea5e99cc416a21298f27b98bb70157c8db29e3a
ishworii <ishworkhanal21@gmail.com>
2024-04-07 14:29:34 +0545

    Updated to use undetected_chromedriver

--- colabctl.py
+++ colabctl.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.insert(0,'/usr/lib/chromium-browser/chromedriver')
+
+# sys.path.insert(0,'/usr/lib/chromium-browser/chromedriver')
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
@@ -11,6 +12,9 @@ from selenium.webdriver.support import expected_conditions as EC
 import pickle
 import time
 import validators
+import undetected_chromedriver as uc
+
+CHROMEWEBDRIVER = "/home/ik-pc/Desktop/upwork/Juan/colabctl/chromedriver"
 
 
 def sleep(seconds):
@@ -23,7 +27,11 @@ def sleep(seconds):
 
 def exists_by_text2(driver, text):
     try:
-        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH,"//*[contains(text(), '"+str(text)+"')]")))
+        WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located(
+                (By.XPATH, "//*[contains(text(), '" + str(text) + "')]")
+            )
+        )
     except Exception:
         return False
     return True
@@ -31,7 +39,9 @@ def exists_by_text2(driver, text):
 
 def exists_by_xpath(driver, thex, howlong):
     try:
-        WebDriverWait(driver, howlong).until(ec.visibility_of_element_located((By.XPATH, thex)))
+        WebDriverWait(driver, howlong).until(
+            EC.visibility_of_element_located((By.XPATH, thex))
+        )
     except:
         return False
 
@@ -39,7 +49,7 @@ def exists_by_xpath(driver, thex, howlong):
 def exists_by_text(driver, text):
     driver.implicitly_wait(2)
     try:
-        driver.find_element_by_xpath("//*[contains(text(), '"+str(text)+"')]")
+        driver.find_element(By.XPATH, "//*[contains(text(), '" + str(text) + "')]")
     except NoSuchElementException:
         driver.implicitly_wait(5)
         return False
@@ -49,7 +59,7 @@ def exists_by_text(driver, text):
 
 def user_logged_in(driver):
     try:
-        driver.find_element_by_xpath('//*[@id="file-type"]')
+        driver.find_element(By.XPATH, '//*[@id="file-type"]')
     except NoSuchElementException:
         driver.implicitly_wait(5)
         return False
@@ -60,7 +70,7 @@ def user_logged_in(driver):
 def wait_for_xpath(driver, x):
     while True:
         try:
-            driver.find_element_by_xpath(x)
+            driver.find_element(By.XPATH, x)
             return True
         except:
             time.sleep(0.1)
@@ -109,23 +119,23 @@ def new_tab(driver, url, tab_index):
         return False
     switch_to_tab(driver, tab_index)
     return True
-    
+
 
 fork = sys.argv[1]
 timeout = int(sys.argv[2])
-colab_urls = file_to_list('notebooks.csv')
+colab_urls = file_to_list("notebooks.csv")
 
 if len(colab_urls) > 0 and validators.url(colab_urls[0]):
     colab_1 = colab_urls[0]
 else:
-    raise Exception('No notebooks')
+    raise Exception("No notebooks")
 
-chrome_options = Options()
-chrome_options.add_argument('--headless') # uncomment for headless mode
-chrome_options.add_argument('--no-sandbox')
-#chrome_options.add_argument("user-data-dir=profile") # left for debugging
-chrome_options.add_argument('--disable-dev-shm-usage')
-wd = webdriver.Chrome('chromedriver', options=chrome_options)
+chrome_options = uc.ChromeOptions()
+# chrome_options.add_argument("--headless")  # uncomment for headless mode
+chrome_options.add_argument("--no-sandbox")
+chrome_options.user_data_dir = "profile"  # left for debugging
+chrome_options.add_argument("--disable-dev-shm-usage")
+wd = uc.Chrome(options=chrome_options)
 
 wd.get(colab_1)
 try:
@@ -139,68 +149,85 @@ if exists_by_text(wd, "Sign in"):
     print("No auth cookie detected. Please login to Google.")
     wd.close()
     wd.quit()
-    chrome_options_gui = Options()
-    chrome_options_gui.add_argument('--no-sandbox')
-    #chrome_options.add_argument("user-data-dir=profile") # left for debugging
-    chrome_options_gui.add_argument('--disable-infobars')
-    wd = webdriver.Chrome('chromedriver', options=chrome_options_gui)
+    chrome_options_gui = uc.ChromeOptions()
+    chrome_options_gui.add_argument("--no-sandbox")
+    chrome_options_gui.user_data_dir = "profile"  # left for debugging
+    chrome_options_gui.add_argument("--disable-infobars")
+    wd = uc.Chrome(options=chrome_options_gui)
     wd.get("https://accounts.google.com/signin")
-    wait_for_xpath(wd, '//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/c-wiz/div/div[4]/div/div/header/div[2]')
+    wait_for_xpath(
+        wd,
+        '//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/c-wiz/c-wiz/div/div[3]/div/div/header/div[2]',
+    )
     print("Login detected. Saving cookie & restarting connection.")
     pickle.dump(wd.get_cookies(), open("gCookies.pkl", "wb"))
     wd.close()
     wd.quit()
-    wd = webdriver.Chrome('chromedriver', options=chrome_options)
+
+    chrome_options2 = uc.ChromeOptions()
+    # chrome_options.add_argument("--headless")  # uncomment for headless mode
+    chrome_options2.add_argument("--no-sandbox")
+    chrome_options2.user_data_dir = "profile"  # left for debugging
+    chrome_options2.add_argument("--disable-dev-shm-usage")
+    wd = uc.Chrome(options=chrome_options2)
 while True:
     for colab_url in colab_urls:
         complete = False
         wd.get(colab_url)
-        print("Logged in.") # for debugging
+        print("Logged in.")  # for debugging
         running = False
         wait_for_xpath(wd, '//*[@id="file-menu-button"]/div/div/div[1]')
-        print('Notebook loaded.')
+        print("Notebook loaded.")
         sleep(10)
 
         while not exists_by_text(wd, "Sign in"):
             if exists_by_text(wd, "Runtime disconnected"):
                 try:
-                    wd.find_element_by_xpath('//*[@id="ok"]').click()
+                    wd.find_element(By.XPATH, '//*[@id="ok"]').click()
                 except NoSuchElementException:
                     pass
             if exists_by_text2(wd, "Notebook loading error"):
                 wd.get(colab_url)
             try:
-                wd.find_element_by_xpath('//*[@id="file-menu-button"]/div/div/div[1]')
+                wd.find_element(By.XPATH, '//*[@id="file-menu-button"]/div/div/div[1]')
                 if not running:
-                    wd.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.SHIFT + "q")
-                    wd.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.SHIFT + "k")
+                    wd.find_element(By.TAG_NAME, "body").send_keys(
+                        Keys.CONTROL + Keys.SHIFT + "q"
+                    )
+                    wd.find_element(By.TAG_NAME, "body").send_keys(
+                        Keys.CONTROL + Keys.SHIFT + "k"
+                    )
                     exists_by_xpath(wd, '//*[@id="ok"]', 10)
-                    wd.find_element_by_xpath('//*[@id="ok"]').click()
+                    wd.find_element(By.XPATH, '//*[@id="ok"]').click()
                     sleep(10)
-                    wd.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.F9)
+                    wd.find_element(By.TAG_NAME, "body").send_keys(
+                        Keys.CONTROL + Keys.F9
+                    )
                     running = True
             except NoSuchElementException:
                 pass
             if running:
                 try:
-                    wd.find_element_by_css_selector('.notebook-content-background').click()
-                    #actions = ActionChains(wd)
-                    #actions.send_keys(Keys.SPACE).perform()
+                    wd.find_element(
+                        By.CSS_SELECTOR, ".notebook-content-background"
+                    ).click()
+                    # actions = ActionChains(wd)
+                    # actions.send_keys(Keys.SPACE).perform()
                     scroll_to_bottom(wd)
                     print("performed scroll")
                 except:
                     pass
-                for frame in wd.find_elements_by_tag_name('iframe'):
+                for frame in wd.find_elements(By.TAG_NAME, "iframe"):
                     wd.switch_to.frame(frame)
-                    '''
+                    """
                     links = browser.find_elements_by_partial_link_text('oauth2/auth')
                     for link in links:
                         new_tab(wd, link.get_attribute("href"), 1)
                         wd.find_element_by_css_selector('li.M8HEDc:nth-child(1)>div:nth-child(1)').click()
                         wd.find_element_by_css_selector('#submit_approve_access>content:nth-child(3)>span:nth-child(1)').click()
                         auth_code = wd.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div[2]/div/div/div[2]/div/div/div/form/content/section/div/content/div/div/div/textarea').text
-                    '''
-                    for output in wd.find_elements_by_tag_name('pre'):
+                    """
+                    for output in wd.find_element(By.TAG_NAME, "pre"):
                         if fork in output.text:
                             running = False
                             complete = True
