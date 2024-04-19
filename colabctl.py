4e00945b3834348df414cbaeaa9922fd22a84005
ishworii <ishworkhanal21@gmail.com>
2024-04-20 01:28:18 +0545

    Fixed bugs,added logger

--- colabctl.py
+++ colabctl.py
@@ -1,20 +1,40 @@
 import sys
-
-# sys.path.insert(0,'/usr/lib/chromium-browser/chromedriver')
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import pickle
 import time
 import validators
 import undetected_chromedriver as uc
+import os
+import argparse
+import logging
 
-CHROMEWEBDRIVER = "/home/ik-pc/Desktop/upwork/Juan/colabctl/chromedriver"
+USER_PROFILE_DIRECTORY = "profile"
+BASE_URL = "https://www.google.com/"
+
+
+def setup_logger(name, log_file, level=logging.INFO, console_handler=True):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    if console_handler:
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
+    else:
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    return logger
+
+
+# setup logger
+logger = setup_logger("colabctl", "colabctl.log", console_handler=False)
 
 
 def sleep(seconds):
@@ -22,19 +42,7 @@ def sleep(seconds):
         try:
             time.sleep(1)
         except KeyboardInterrupt:
-            continue
-
-
-def exists_by_text2(driver, text):
-    try:
-        WebDriverWait(driver, 2).until(
-            EC.presence_of_element_located(
-                (By.XPATH, "//*[contains(text(), '" + str(text) + "')]")
-            )
-        )
-    except Exception:
-        return False
-    return True
+            return
 
 
 def exists_by_xpath(driver, thex, howlong):
@@ -42,50 +50,33 @@ def exists_by_xpath(driver, thex, howlong):
         WebDriverWait(driver, howlong).until(
             EC.visibility_of_element_located((By.XPATH, thex))
         )
-    except:
+        return True
+    except (NoSuchElementException, TimeoutException) as e:
+        logger.warning(f"the element not found by xpath {thex} after waiting for {howlong}")
         return False
 
 
-def exists_by_text(driver, text):
-    driver.implicitly_wait(2)
-    try:
-        driver.find_element(By.XPATH, "//*[contains(text(), '" + str(text) + "')]")
-    except NoSuchElementException:
-        driver.implicitly_wait(5)
-        return False
-    driver.implicitly_wait(5)
-    return True
-
-
-def user_logged_in(driver):
-    try:
-        driver.find_element(By.XPATH, '//*[@id="file-type"]')
-    except NoSuchElementException:
-        driver.implicitly_wait(5)
-        return False
-    driver.implicitly_wait(5)
-    return True
-
-
-def wait_for_xpath(driver, x):
+def wait_for_xpath(driver, x, timeout=100):
+    start_time = time.time()
     while True:
         try:
-            driver.find_element(By.XPATH, x)
-            return True
+            element = driver.find_element(By.XPATH, x)
+            if element:
+                return True
         except:
+            elapsed_time = time.time() - start_time
+            if elapsed_time > timeout:
+                return False
             time.sleep(0.1)
-            pass
 
 
-def scroll_to_bottom(driver):
-    SCROLL_PAUSE_TIME = 0.5
-    # Get scroll height
+def scroll_to_bottom(driver, scroll_pause_time=0.5):
     last_height = driver.execute_script("return document.body.scrollHeight")
     while True:
         # Scroll down to bottom
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         # Wait to load page
-        time.sleep(SCROLL_PAUSE_TIME)
+        time.sleep(scroll_pause_time)
         # Calculate new scroll height and compare with last scroll height
         new_height = driver.execute_script("return document.body.scrollHeight")
         if new_height == last_height:
@@ -95,147 +86,143 @@ def scroll_to_bottom(driver):
 
 def file_to_list(filename):
     colabs = []
-    for line in open(filename):
-        if validators.url(line):
-            colabs.append(line)
+    try:
+        with open(filename, 'r') as file:
+            for line in file:
+                clean_line = line.strip()
+                if validators.url(clean_line):
+                    colabs.append(clean_line)
+    except IOError as e:
+        logger.critical(f"Error opening or reading the file: {e}")
     return colabs
 
 
-def switch_to_tab(driver, tab_index):
-    print("Switching to tab " + str(tab_index))
-    try:
-        driver.switch_to.window(driver.window_handles[tab_index])
-    except:
-        print("Error switching tabs.")
-        return False
-
-
-def new_tab(driver, url, tab_index):
-    print("Opening new tab to " + str(url))
-    try:
-        driver.execute_script("window.open('" + str(url) + "')")
-    except:
-        print("Error opening new tab.")
-        return False
-    switch_to_tab(driver, tab_index)
-    return True
-
-
-fork = sys.argv[1]
-timeout = int(sys.argv[2])
-colab_urls = file_to_list("notebooks.csv")
-
-if len(colab_urls) > 0 and validators.url(colab_urls[0]):
-    colab_1 = colab_urls[0]
-else:
-    raise Exception("No notebooks")
-
-chrome_options = uc.ChromeOptions()
-chrome_options.add_argument("--headless")  # uncomment for headless mode
-chrome_options.add_argument("--no-sandbox")
-chrome_options.user_data_dir = "profile"  # left for debugging
-chrome_options.add_argument("--disable-dev-shm-usage")
-wd = uc.Chrome(options=chrome_options)
-
-wd.get(colab_1)
-try:
-    for cookie in pickle.load(open("gCookies.pkl", "rb")):
-        wd.add_cookie(cookie)
-except Exception:
-    pass
-wd.get(colab_1)
-
-if exists_by_text(wd, "Sign in"):
-    print("No auth cookie detected. Please login to Google.")
-    wd.close()
-    wd.quit()
+def wait_for_login(profile_directory):
     chrome_options_gui = uc.ChromeOptions()
     chrome_options_gui.add_argument("--no-sandbox")
-    chrome_options_gui.user_data_dir = "profile"  # left for debugging
     chrome_options_gui.add_argument("--disable-infobars")
-    wd = uc.Chrome(options=chrome_options_gui)
-    wd.get("https://accounts.google.com/signin")
+    chrome_options_gui.add_argument(f"--user-data-dir={profile_directory}")
+    driver = uc.Chrome(options=chrome_options_gui)
+    driver.get("https://accounts.google.com/signin")
     wait_for_xpath(
-        wd,
+        driver,
         '//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/c-wiz/c-wiz/div/div[3]/div/div/header/div[2]',
     )
-    print("Login detected. Saving cookie & restarting connection.")
-    pickle.dump(wd.get_cookies(), open("gCookies.pkl", "wb"))
-    wd.close()
-    wd.quit()
+    driver.close()
+    driver.quit()
 
-    chrome_options2 = uc.ChromeOptions()
-    chrome_options.add_argument("--headless")  # uncomment for headless mode
-    chrome_options2.add_argument("--no-sandbox")
-    chrome_options2.user_data_dir = "profile"  # left for debugging
-    chrome_options2.add_argument("--disable-dev-shm-usage")
-    wd = uc.Chrome(options=chrome_options2)
-while True:
-    for colab_url in colab_urls:
-        complete = False
-        wd.get(colab_url)
-        print("Logged in.")  # for debugging
-        running = False
-        wait_for_xpath(wd, '//*[@id="file-menu-button"]/div/div/div[1]')
-        print("Notebook loaded.")
+
+def handle_login(profile_directory, test_colab_url):
+    if not os.path.isdir(profile_directory):
+        logger.warning(f"Profile directory {profile_directory} does not exist...please login")
+        # login now
+        wait_for_login(profile_directory)
+    else:
+        logger.info("User profile directory found..trying to load first url for test")
+        chrome_options = uc.ChromeOptions()
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-infobars")
+        driver = uc.Chrome(options=chrome_options, user_data_dir=profile_directory)
+        driver.get(test_colab_url)
         sleep(10)
+        # check if the request access is asked
+        if 'Access Denied' in driver.title:
+            logger.error(f"Access Denied to the colab url {test_colab_url},maybe you don't have access to this colab")
+        file_found = wait_for_xpath(driver, '//*[@id="file-menu-button"]/div/div/div[1]')
+        if file_found:
+            logger.info("File found in menu bar...")
+            logger.info("Login detected...restarting and starting new session")
+        else:
+            logger.critical("Loding error,please restart the script...")
+        driver.close()
+        driver.quit()
 
-        while not exists_by_text(wd, "Sign in"):
-            if exists_by_text(wd, "Runtime disconnected"):
-                try:
-                    wd.find_element(By.XPATH, '//*[@id="ok"]').click()
-                except NoSuchElementException:
-                    pass
-            if exists_by_text2(wd, "Notebook loading error"):
-                wd.get(colab_url)
+
+def main():
+    parser = argparse.ArgumentParser(description="Colab cron job")
+    parser.add_argument("fork", type=str, help="Identifier for completion")
+    parser.add_argument("timeout", type=int, help="Timeout in seconds")
+    parser.add_argument("--filename", type=str, default="notebooks.csv", help="File containing colab URLs")
+
+    args = parser.parse_args()
+
+    colab_urls = file_to_list(args.filename)
+    if len(colab_urls) == 0:
+        logger.critical("No valid notebook URLs found")
+        raise Exception("No valid notebook URLs found")
+
+    handle_login(profile_directory=USER_PROFILE_DIRECTORY, test_colab_url=colab_urls[0])
+
+    chrome_options = uc.ChromeOptions()
+    # chrome_options.add_argument("--headless")  # uncomment for headless mode
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    wd = uc.Chrome(options=chrome_options, user_data_dir=USER_PROFILE_DIRECTORY)
+    wd.get(BASE_URL)
+    while True:
+        for each_url in colab_urls:
+            # switch to new tab
+            wd.switch_to.new_window()
+            sleep(5)
+            wd.get(each_url)
+            running = False
+            wait_for_xpath(wd, '//*[@id="file-menu-button"]/div/div/div[1]')  # find 'File' option
+            logger.info(f"Notebook loaded...{each_url}")
+            sleep(10)
             try:
-                wd.find_element(By.XPATH, '//*[@id="file-menu-button"]/div/div/div[1]')
                 if not running:
+                    # clear all cell outputs
+                    logger.info("Clearing all cell outputs")
                     wd.find_element(By.TAG_NAME, "body").send_keys(
-                        Keys.CONTROL + Keys.SHIFT + "q"
+
+                        Keys.CONTROL + Keys.SHIFT + "Q"
                     )
+                    sleep(3)
+
+                    # reset all runtimes
                     wd.find_element(By.TAG_NAME, "body").send_keys(
-                        Keys.CONTROL + Keys.SHIFT + "k"
+                        Keys.CONTROL + Keys.SHIFT + "K"
                     )
-                    exists_by_xpath(wd, '//*[@id="ok"]', 10)
-                    wd.find_element(By.XPATH, '//*[@id="ok"]').click()
-                    sleep(10)
+                    sleep(3)
+                    # click on 'Ok' for resetting the runtime
+                    if exists_by_xpath(wd, '//mwc-dialog[@class="yes-no-dialog"]', 10):
+                        logger.info("Resetting all runtimes")
+                        ok_button = wd.find_element(By.XPATH,
+                                                    '//mwc-dialog[@class="yes-no-dialog"]//mwc-button['
+                                                    '@dialogaction="ok"]')
+
+                        ok_button.click()
+
+                    sleep(5)
+                    # run all cells
+                    logger.info("Running all cells...")
                     wd.find_element(By.TAG_NAME, "body").send_keys(
-                        Keys.CONTROL + Keys.F9
+                        Keys.CONTROL + Keys.SHIFT + 'L'
                     )
                     running = True
-            except NoSuchElementException:
-                pass
-            if running:
+            except NoSuchElementException as e:
+                logger.warning(f"Error while clearing/resetting/running all cells :{e}")
+            while running:
                 try:
                     wd.find_element(
                         By.CSS_SELECTOR, ".notebook-content-background"
                     ).click()
-                    # actions = ActionChains(wd)
-                    # actions.send_keys(Keys.SPACE).perform()
+
                     scroll_to_bottom(wd)
-                    print("performed scroll")
-                except:
-                    pass
-                for frame in wd.find_elements(By.TAG_NAME, "iframe"):
-                    wd.switch_to.frame(frame)
-                    """
-                    links = browser.find_elements_by_partial_link_text('oauth2/auth')
-                    for link in links:
-                        new_tab(wd, link.get_attribute("href"), 1)
-                        wd.find_element_by_css_selector('li.M8HEDc:nth-child(1)>div:nth-child(1)').click()
-                        wd.find_element_by_css_selector('#submit_approve_access>content:nth-child(3)>span:nth-child(1)').click()
-                        auth_code = wd.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div[2]/div/div/div[2]/div/div/div/form/content/section/div/content/div/div/div/textarea').text
-                    """
-                    for output in wd.find_element(By.TAG_NAME, "pre"):
-                        if fork in output.text:
-                            running = False
-                            complete = True
-                            print("Completion string found. Waiting for next cycle.")
-                            break
-                    wd.switch_to.default_content()
-                    if complete:
+                except Exception as e:
+                    logger.warning(f"Error while scrolling to the buttom: {e}")
+                cell_outputs = wd.find_elements(By.XPATH, "//pre")
+                for output in cell_outputs:
+                    if args.fork in output.text:
+                        running = False
+                        logger.info("Completion string found. Waiting for next cycle.")
                         break
-                if complete:
-                    break
-    sleep(timeout)
+            wd.close()
+            wd.switch_to.window(wd.window_handles[0])
+
+        logger.info(f"Sleeping for {args.timeout}")
+        sleep(args.timeout)
+
+
+if __name__ == "__main__":
+    main()
